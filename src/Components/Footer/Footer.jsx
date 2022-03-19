bc789d435297122b0da24a82335a1ccf2f7754c2
Saad Khan <Khan.ji4028@gmail.com>
2022-03-19 22:01:57 +0500

    product details

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,131 +1,64 @@
-import {
-  Facebook,
-  Instagram,
-  MailOutline,
-  Phone,
-  Pinterest,
-  Twitter,
-} from "@mui/icons-material";
-import styled from "styled-components";
-
-const Container = styled.div`
-  display: flex;
-`;
-
-const Left = styled.div`
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  padding: 20px;
-`;
-
-const Logo = styled.h1``;
-
-const Desc = styled.p`
-  margin: 10px 0px;
-`;
-
-const SocialContainer = styled.div`
-  display: flex;
-`;
-
-const SocialIcon = styled.div`
-  width: 40px;
-  height: 40px;
-  border-radius: 50%;
-  color: white;
-  background-color: #${(props) => props.color};
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin-right: 20px;
-`;
-
-const Center = styled.div`
-  flex: 1;
-  padding: 20px;
-`;
-
-const Title = styled.h3`
-  margin-bottom: 30px;
-`;
-
-const List = styled.ul`
-  margin: 0;
-  padding: 0;
-  list-style: none;
-  display: flex;
-  flex-wrap: wrap;
-`;
-
-const ListItem = styled.li`
-  width: 50%;
-  margin-bottom: 10px;
-`;
-
-const Right = styled.div`
-  flex: 1;
-  padding: 20px;
-`;
-
-const ContactItem = styled.div`
-  margin-bottom: 20px;
-  display: flex;
-  align-items: center;
-`;
-
-
-const Footer = () => {
+import React from "react";
+import "./Footer.css";
+export default function Footer() {
   return (
-    <Container>
-      <Left>
-        <Logo>Virtual Market</Logo>
-        <Desc>
-          There are many variations of passages of Lorem Ipsum available, but
-            the majority have suffered alteration in some form, by injected
-          humour, or randomised words which don’t look even slightly believable.
-        </Desc>
-        <SocialContainer>
-          <SocialIcon color="3B5999">
-            <Facebook />
-          </SocialIcon>
-          <SocialIcon color="E4405F">
-            <Instagram />
-          </SocialIcon>
-          <SocialIcon color="55ACEE">
-            <Twitter />
-          </SocialIcon>
-          <SocialIcon color="E60023">
-            <Pinterest />
-          </SocialIcon>
-        </SocialContainer>
-      </Left>
-      <Center>
-        <Title>Useful Links</Title>
-        <List>
-          <ListItem>Home</ListItem>
-          <ListItem>Cart</ListItem>
-          <ListItem>Man Fashion</ListItem>
-          <ListItem>Woman Fashion</ListItem>
-          <ListItem>Accessories</ListItem>
-          <ListItem>My Account</ListItem>
-          <ListItem>Order Tracking</ListItem>
-          <ListItem>Wishlist</ListItem>
-          <ListItem>Wishlist</ListItem>
-          <ListItem>Terms</ListItem>
-        </List>
-      </Center>
-      <Right>
-        <Title>Contact</Title>
-        <ContactItem>
-          <Phone style={{ marginRight: "10px" }} /> +92 300 68601
-        </ContactItem>
-        <ContactItem>
-          <MailOutline style={{ marginRight: "10px" }} /> contact@VirtualMarket.com
-        </ContactItem>
-      </Right>
-    </Container>
-  );
-};
+    <div className="Footer">
+      <footer class="footer-distributed">
+        <div class="footer-left">
+          <h3>
+            <span className="logo">
+              Virtual<span className="logoinner">Market</span>
+            </span>
+          </h3>
 
-export default Footer;
+          <p class="footer-company-name">© 2022 Virtual Market Pvt. Ltd.</p>
+        </div>
+
+        <div class="footer-center">
+          <div>
+            <i class="fa fa-map-marker"></i>
+            <p>
+              230, Cavalry Ground
+              <br></br>
+              Lahore Cantt - 54810
+            </p>
+          </div>
+
+          <div>
+            <i class="fa fa-phone"></i>
+            <p>+92 37865432</p>
+          </div>
+          <div>
+            <i class="fa fa-envelope"></i>
+            <p>
+              <a href="mailto:support@eduonix.com">support@virtualmarket.com</a>
+            </p>
+          </div>
+        </div>
+        <div class="footer-right">
+          <p class="footer-company-about">
+            <span>About the company</span>
+            We offer best products and wholesale dealers across Pakistan.
+          </p>
+          <div class="footer-icons">
+            <a href="#">
+              <i class=""></i>
+            </a>
+            <a href="#">
+              <i class=""></i>
+            </a>
+            <a href="#">
+              <i class=""></i>
+            </a>
+            <a href="#">
+              <i class=""></i>
+            </a>
+            <a href="#">
+              <i class=""></i>
+            </a>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+}
--- src/Components/ProductDetail/ProductDetail.jsx
+++ src/Components/ProductDetail/ProductDetail.jsx
@@ -5,101 +5,11 @@ import { makeStyles } from "@mui/styles";
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 import productService from "../../Services/ProductServices";
+import { ArrowForwardIos, Home } from "@mui/icons-material";
+import { TextField } from "@material-ui/core";
+import "./ProductDetail.css";
 
-const Container = styled.div``;
-
-const Wrapper = styled.div`
-  padding: 50px;
-  display: flex;
-`;
-
-const ImgContainer = styled.div`
-  flex: 1;
-`;
-
-const Image = styled.img`
-  width: 100%;
-  height: 90vh;
-  object-fit: cover;
-`;
-
-const InfoContainer = styled.div`
-  flex: 1;
-  padding: 0px 50px;
-`;
-
-const Title = styled.h1`
-  font-weight: 200;
-`;
-
-const Desc = styled.p`
-  margin: 20px 0px;
-`;
-
-const Price = styled.span`
-  font-weight: 100;
-  font-size: 40px;
-`;
-
-const FilterContainer = styled.div`
-  width: 50%;
-  margin: 30px 0px;
-  display: flex;
-  justify-content: space-between;
-`;
-
-const AddContainer = styled.div`
-  width: 50%;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-`;
-
-const AmountContainer = styled.div`
-  display: flex;
-  align-items: center;
-  font-weight: 700;
-`;
-
-const Amount = styled.span`
-  width: 30px;
-  height: 30px;
-  border-radius: 10px;
-  border: 1px solid teal;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin: 0px 5px;
-`;
-
-const Button = styled.button`
-  padding: 15px;
-  border: 2px solid teal;
-  background-color: white;
-  cursor: pointer;
-  font-weight: 500;
-  &:hover {
-    background-color: #f8f4f4;
-  }
-`;
-const useStyles = makeStyles({
-  root: {
-    display: "flex",
-    maxWidth: 500,
-    margin: 20,
-    alignItems: "center",
-    justifyContent: "space-between",
-  },
-  media: {
-    height: 140,
-  },
-  imageBox: {
-    width: 500,
-    height: 500,
-  },
-});
 const ProductDetail = (props) => {
-  const classes = useStyles();
   const [productDetails, SetProductDetails] = useState("");
   const _id = props.match.params.id;
   console.log(_id);
@@ -114,38 +24,105 @@ const ProductDetail = (props) => {
       });
   };
   useEffect(getDetails, []);
-  console.log(productDetails);
+  console.log(productDetails.images);
   return (
-    <Container>
-      <Wrapper className={classes.root}>
-        {productDetails ? (
-          <>
-            <Card>
-              <Image
-                className={classes.imageBox}
-                src={productDetails.images[0].link}
-              />
-            </Card>
-          </>
-        ) : (
-          <></>
-        )}
-        <InfoContainer>
-          <Title></Title>
-          <Desc>{productDetails.description}</Desc>
-          <Price>${productDetails.price}</Price>
-          <FilterContainer></FilterContainer>
-          <AddContainer>
-            <AmountContainer>
-              <Remove />
-              <Amount>1</Amount>
-              <Add />
-            </AmountContainer>
-            <Button>ADD TO CART</Button>
-          </AddContainer>
-        </InfoContainer>
-      </Wrapper>
-    </Container>
+    <div>
+      {productDetails ? (
+        <div>
+          <div className="DetailPage">
+            <div className="navigationBar">
+              <nav className="navigation">
+                <label className="labelIcon">
+                  <Home />
+                  Home
+                </label>
+
+                <label className="arrowIcon">
+                  <ArrowForwardIos />
+                </label>
+
+                <label className="labelIcon">
+                  <a className="title" href="#">
+                    {productDetails.name}
+                  </a>
+                </label>
+              </nav>
+            </div>
+            <div className="mainContainer">
+              <div className="productPreview">
+                <img
+                  src={productDetails.images[0].link}
+                  alt="Product Preview"
+                />
+                {productDetails.images.map((image, index) => {
+                  return (
+                    <div key={image}>
+                      <img src={image} alt="" className="sellerUpdateImg" />
+                    </div>
+                  );
+                })}
+              </div>
+              <div className="productData">
+                <h1 className="productTitle">Samsung S21 Ultra</h1>
+                <p className="productPrice">Rs {productDetails.price}</p>
+                <p className="productDescription">
+                  {productDetails.description}
+                  <p className="productDescription">
+                    Processor Samsung Exynos 2100
+                  </p>
+                  <p className="productDescription">Front Camera 40MP</p>
+                  <p className="productDescription">
+                    Rear Camera 108MP + 12MP + 10MP + 10MP
+                  </p>
+                  <p className="productDescription">
+                    RAM 12GB Storage 128GB
+                    <p className="productDescription">
+                      Battery Capacity 5000mAh OS Android 11
+                    </p>
+                  </p>
+                </p>
+
+                <h3 className="sectionHeading">Select Quantity</h3>
+                <div>
+                  <TextField
+                    id="outlined-number"
+                    label="Quantity"
+                    type="number"
+                    required
+                    min={productDetails.minOrder}
+                    placeholder="e.g. 1"
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                    variant="outlined"
+                  />
+
+                  <h3 className="sectionHeading"></h3>
+                  <div>
+                    <button className="featureItem">Add To Cart</button>
+                    <button className="featureItem2">Bargain</button>
+
+                    {productDetails.sampleOrder ? (
+                      <>
+                        <button className="featureItem2">Sample Order</button>
+                      </>
+                    ) : (
+                      <></>
+                    )}
+                  </div>
+                </div>
+                <div className="sellerInfo">
+                  <p>Sold By:</p>
+                  <h3>{productDetails.seller.storeName}</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div></div>
+      )}
+    </div>
   );
 };
 
